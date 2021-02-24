de223f7946f6a0734afb2cea88dbf7082eff4f43
VicDebonne <vic.debonne@student.howest.be>
2021-02-24 11:16:30 +0100

    add game title

--- ui.js
+++ ui.js
@@ -1,6 +1,24 @@
 "use strict";
 const React = require("react");
 const { useInput, useApp, Text, Box } = require("ink");
+// const BigText = require("ink-big-text");
+// const Gradient = require("ink-gradient");
+const CFonts = require('cfonts');
+
+CFonts.say('Snake Deluxe', {
+	font: 'block',
+	align: 'center',
+	colors: ['red'],
+	background: 'black',
+	letterSpacing: 1,
+	lineHeight: 4,
+	space: true,
+	maxLength: '0',
+	gradient: ['red', 'blue'],
+	independentGradient: false,
+	transitionGradient: false,
+	env: 'node'
+});
 
 const App = () => {
     const { exit } = useApp();
@@ -31,6 +49,9 @@ const App = () => {
 
     return (
         <Box flexDirection="column">
+			{/* <Gradient name="rainbow">
+				<BigText text="snake deluxe"/>
+			</Gradient> */}
             <Text>Use arrow keys to move the icon. Press "q" to exit.</Text>
             <Box height={12} paddingLeft={x} paddingTop={y}>
                 <Text>😁</Text>
